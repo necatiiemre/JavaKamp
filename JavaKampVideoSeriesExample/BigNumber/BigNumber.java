a5b503433af26618cdecade61810c05731b7ded2
Emre Özdemir <64666072+necatiiemre@users.noreply.github.com>
2022-09-28 22:44:14 +0300

    Add files via upload

---
+++ JavaKampVideoSeriesExample/BigNumber/BigNumber.java
@@ -0,0 +1,34 @@
+//Emre Ozdemir
+//20050111020
+
+import java.util.Scanner;
+
+public class BigNumber{
+	public static void main(String[] args){
+		int number1,
+		number2,
+		number3,
+		biggerNumber;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a number1 : ");
+		number1=input.nextInt();
+		System.out.println("Please enter a number2 : ");
+		number2=input.nextInt();
+		System.out.println("Please enter a number3 : ");
+		number3=input.nextInt();
+		
+		biggerNumber=number1;
+		
+		if(biggerNumber<number2){
+		biggerNumber = number2;
+		}
+		if(biggerNumber<number3){
+		biggerNumber = number3;
+		}
+		
+		System.out.println("BigNumber is :" + biggerNumber);
+		
+		
+	}
+}
---
+++ JavaKampVideoSeriesExample/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+//Emre Ozdemir
+//20050111020
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello word!");
+	}
+}
